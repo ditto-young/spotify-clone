cc16cdacda9db325bbb507efe488206d402271ce
gpdud0243 <gpdud0243@naver.com>
2021-03-05 21:05:24 +0900

    modified

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player';
+import {useDataLayerValue} from './DataLayer';
+
+
+const spotify = new SpotifyWebApi();
+
 
 function App() {
-  const [token, setToken] = useState(null);
+  const [{user, token}, dispatch] = useDataLayerValue();
+
 
   //Run code base on given condition
   useEffect(() => {
@@ -13,19 +21,33 @@ function App() {
     const _token = hash.access_token;
 
     if(_token){
-      setToken(_token);
+      dispatch({
+        type:"SET_TOKEN",
+        token: _token,
+      });
+      
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
+      });
     }
 
     console.log("I HAVE A TOKEN >>>", token);
   },[]);
+
+  console.log("smile",user);
+  console.log("alien",token);
 
   return (
     //BEM
     <div className="app">
       {
         token ? (
-            <h1>I am logged</h1>
-            //<Player />
+            <Player/>
         ) : (
             <Login/>
         )
