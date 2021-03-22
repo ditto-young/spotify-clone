83dd15c82733943dc3758e2a17f11f9d4db3b30a
gpdud0243 <gpdud0243@naver.com>
2021-03-22 19:28:04 +0900

    new

--- src/App.js
+++ src/App.js
@@ -41,6 +41,13 @@ function App() {
           playlists: playlists,
         });
       });
+
+      spotify.getPlaylist("37i9d0ZEVXcIJazRV9ISoM").then(response => 
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      );
     }
   },[]);
 
--- src/Body.js
+++ src/Body.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import './Body.css';
 import Header from './Header';
+import { useStateValue } from './StateProvider';
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
 
 function Body({spotify}) {
+    const [{ discover_weekly }, dispatch] = useStateValue();
+
     return (
         <div className="body">
             <Header spotify={spotify} />
+            <div className="body__info">
+                <img 
+                    src="https://www.hypebot.com/wp-content/uploads/2020/07/discover-weekly.png"
+                    //{discover_weekly?.images[0].url}
+                    alt=""
+                />
+                <div className="body__infoText">
+                    <strong>PLAYLIST</strong>
+                    <h2>Discover Weekly</h2>
+                    <p>{discover_weekly?.description}</p>
+                </div>
+            </div>
+            <div className="body__songs">
+                <div className="body__icons">
+                    <PlayCircleFilledIcon/>
+                    <FavoriteIcon/>
+                    <MoreHorizIcon/>
+                </div>
+            </div>
+
         </div>
     );
 }
